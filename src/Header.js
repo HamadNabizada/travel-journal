de3a8c4eeee5d97397bff20adaa7657759e55d94
Hamad <HamadNabizada@gmail.com>
2022-04-12 21:16:32 -0700

    created initial files. completed header component

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+        <div className="header-wrapper">
+            <img className='logo' src="/images/logo.svg" alt="" />
+            <h1>my travel journal.</h1>
+        </div>
+    )
+}
