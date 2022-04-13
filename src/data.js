9637e3f380f27d9017902604667ce1f6c6183e64
Hamad <HamadNabizada@gmail.com>
2022-04-12 21:28:55 -0700

    added assets/data

--- src/data.js
+++ src/data.js
@@ -6,6 +6,25 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "mountfuji.jpg"
+    },
+    {
+        title: "Golden Gate Bridge",
+        location: "USA",
+        googleMapsUrl: "https://goo.gl/maps/H5Dyp9EFqM5ySP6c6",
+        startDate: "19 Jan, 2022",
+        endDate: "24 Jan, 2022",
+        description: "This it the Golden Gate Bridge, located in San Francisco, CA.",
+        imageUrl: "goldengate.jpeg"
+    },
+    {
+        title: "Eiffel Tower",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/cM3ZNgtTVK1yS7dJ6",
+        startDate: "03 Dec, 2021",
+        endDate: "08 Dec, 2021",
+        description: "This is the Eiffel Tower, located in Paris, France.",
+        imageUrl: "eiffel.jpg"
     }
+    
 ]
